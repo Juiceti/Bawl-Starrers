7871ba5591aaba3c0b26443a5fa79524af5284b8
VibingYoshi <joshivivaan19@gmail.com>
2024-01-26 17:44:15 -0500

    Movement fixed

--- LuffyR.java
+++ LuffyR.java
@@ -22,28 +22,31 @@ public class LuffyR extends Actor
     public LuffyR(){
         GreenfootImage image = getImage(); image.scale(image.getWidth() /2, image.getHeight() /2); setImage(image);
     }
-    public void checkW(){
-        if (Greenfoot.isKeyDown("W")&& this != null){
-            getWorld().addObject(new LuffyB(),getX(),getY()-2);
-            getWorld().removeObject(this);
+    public void checkW() {
+        if (Greenfoot.isKeyDown("W")||Greenfoot.isKeyDown("up")) {
+            setImage(new LuffyB().getImage());
+            setLocation(getX(),getY()-2);
         }
     }
-    public void checkS(){
-        if (Greenfoot.isKeyDown("S")&& this != null){
-            getWorld().addObject(new LuffyF(),getX(),getY()+2);
-            getWorld().removeObject(this);
+
+    public void checkS() {
+        if (Greenfoot.isKeyDown("S")||Greenfoot.isKeyDown("down")) {
+            setImage(new LuffyF().getImage());
+            setLocation(getX(),getY()+2);
         }
     }
-    public void checkD(){
-        if (Greenfoot.isKeyDown("D")&& this != null){
-            getWorld().addObject(new LuffyR(),getX()+2,getY());
-            getWorld().removeObject(this);
+
+    public void checkD() {
+        if (Greenfoot.isKeyDown("D")||Greenfoot.isKeyDown("right")) {
+            setImage(new LuffyR().getImage());
+            setLocation(getX()+2,getY());
         }
     }
-    public void checkA(){
-        if (Greenfoot.isKeyDown("A")&& this != null){
-            getWorld().addObject(new LuffyL(),getX()-2,getY());
-            getWorld().removeObject(this);
+
+    public void checkA() {
+        if (Greenfoot.isKeyDown("A")) {
+            setImage(new LuffyL().getImage());
+            setLocation(getX()-2,getY());
         }
     }
 }
